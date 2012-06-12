42f03cf7d7345b68bfc482fabfe9a17ec39798f8
Cliff Seal <cliff@logos-creative.com>
2012-06-12 11:03:18 -0400

    Added live tile switching functionality
    Included some commented code to illustrate live tile switching
    capabilities via CSS; added instructions to README.md

---
+++ js/switcher.js
@@ -0,0 +1,41 @@
+﻿// Adapted from: http://www.kelvinluck.com/assets/jquery/styleswitch/toggle.html (extensively modified)
+
+/**
+* Stylesheet toggle variation on styleswitch stylesheet switcher.
+* Built on jQuery.
+* Under an CC Attribution, Share Alike License.
+* By Kelvin Luck ( http://www.kelvinluck.com/ )
+**/
+
+(function($) {
+		// What's up?
+		var availableStylesheets = [];
+		var activeStylesheetIndex = 0;
+		
+		// Switcheroo.
+		$.stylesheetSwitch = function(styleName)
+		{
+			$('link[@rel*=style][title]').each(
+				function(i) 
+				{
+					this.disabled = true;
+					if (this.getAttribute('title') == styleName) {
+						this.disabled = false;
+						activeStylesheetIndex = i;
+					}
+				}
+			);
+			createCookie('style', styleName, 365);
+		};
+		
+		// That's what's up. 
+		$.stylesheetInit = function()
+		{
+			$('link[rel*=style][title]').each(
+				function(i) 
+				{
+					availableStylesheets.push(this.getAttribute('title'));
+				}
+			);
+		};
+})(jQuery);
